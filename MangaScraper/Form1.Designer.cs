277da05834a74d8121e7bd4308e662043a6f84cc
Richard Tran <rntran@ucsd.edu>
2015-02-09 21:54:56 -0800

    Initial Commit

---
+++ MangaScraper/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace JPGScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlBox = new System.Windows.Forms.RichTextBox();
+            this.getJPGButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(61, 54);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(165, 35);
+            this.urlBox.TabIndex = 0;
+            this.urlBox.Text = "";
+            this.urlBox.TextChanged += new System.EventHandler(this.urlBox_TextChanged);
+            // 
+            // getJPGButton
+            // 
+            this.getJPGButton.Location = new System.Drawing.Point(104, 107);
+            this.getJPGButton.Name = "getJPGButton";
+            this.getJPGButton.Size = new System.Drawing.Size(75, 23);
+            this.getJPGButton.TabIndex = 1;
+            this.getJPGButton.Text = "Get Manga!";
+            this.getJPGButton.UseVisualStyleBackColor = true;
+            this.getJPGButton.Click += new System.EventHandler(this.getJPGButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter URL Here!";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(58, 160);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 13);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getJPGButton);
+            this.Controls.Add(this.urlBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox urlBox;
+        private System.Windows.Forms.Button getJPGButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ MangaScraper/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JPGScraper
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void displayStatus(string status)
+        {
+            statusLabel.Text = status;
+        }
+        private string getChapter(string html)
+        {
+            //MessageBox.Show("getting shit");
+            //System.Diagnostics.Debug.Write(html);
+            int index = html.IndexOf("var current_chapter");
+            string chapter = html.Substring(index); // remove first "
+            index = chapter.IndexOf("\"");
+            chapter = chapter.Substring(index + 1); //remove first " c###"
+            index = chapter.IndexOf("\"");
+            chapter = chapter.Substring(0, index); // remove second " now all that is left is c###
+            //System.Diagnostics.Debug.Write(chapter);
+            return chapter;
+        }
+        private string getPage(string html)
+        {
+            int index = html.IndexOf("var current_page");
+            string page = html.Substring(index);
+            index = page.IndexOf(" ");//current_page = _20_;
+            page = page.Substring(index + 1);
+            index = page.IndexOf(" "); // pages are in format _##_ where _ is space
+            page = page.Substring(index + 1);// =_20_
+            index = page.IndexOf(" ");
+            page = page.Substring(index + 1);//20_
+            index = page.IndexOf(" ");
+            page = page.Substring(0, index);//20
+            
+           // System.Diagnostics.Debug.Write(page+"\n");
+            return page;
+        }
+        private string getSeriesName(string html)
+        {
+            int index = html.IndexOf("var series_name");
+            string series = html.Substring(index);
+            index = series.IndexOf("\"");
+            series = series.Substring(index + 1);
+            index = series.IndexOf("\"");
+            series = series.Substring(0, index);
+            return series;
+        }
+        private void downloadJPG(string jpgURL, string series, string chapter, string page)
+        {
+            //download jpgs using this
+              using(WebClient Client = new WebClient())
+              {
+                  //System.Diagnostics.Debug.Write(page+"\n");
+                  string direct = series + "/" + chapter + "/" + page + ".jpg";
+                  displayStatus("Downloading: " + series + " chapter:" + chapter + " page:" + page);
+                  System.Diagnostics.Debug.Write(direct + "\n");
+                  //System.Diagnostics.Debug.Write(jpgURL + "\n");
+                System.IO.Directory.CreateDirectory(series);
+                System.IO.Directory.CreateDirectory(series + "/" + chapter);
+                Client.Proxy = null;
+                  Client.DownloadFile(jpgURL, direct);
+              }
+        }
+
+        private string extractJPGFromHTML(string html)
+        {
+           // string[] split = Regex.Split(html,"<img src=\"");//get the URL but contains ""
+            int index = html.IndexOf("img src=\"");//get first instance of img src=" at where i is
+            html = html.Substring(index + 9); // 9 is for each character of img src="
+            index = html.IndexOf("\""); // remove second " at the end of jpg url
+            html = html.Substring(0, index - 1); 
+            return html;
+        }
+        
+        private string getNextURL(string html)
+        {
+            string htmlBackup = html;
+            int index = html.IndexOf("return next_page();"); // get next page url
+
+            html = html.Substring(0,index); // 45 to get rid of the html, goes straight to the url
+           // System.Diagnostics.Debug.Write(html);
+            index = html.LastIndexOf("a href=\"");
+            html = html.Substring(index + 8); // get rid of a href="
+            index = html.IndexOf("\"");
+            html = html.Substring(0, index);
+            //System.Diagnostics.Debug.Write(html + "\n");
+            if (html == "javascript:void(0);")//if end of chapter, get next chapter
+                html = getNextChapter(htmlBackup);
+            
+            return html;
+        }
+        private string getNextChapter(string html)
+        {
+            int index = html.IndexOf("Next Chapter:");//get index of next chapter url
+            if (index == -1)
+            {
+                return "null";
+            }
+               
+            html = html.Substring(index);
+            index = html.IndexOf("\"");//start html at first "
+            html = html.Substring(index + 1);//get rid of the first "
+            index = html.IndexOf("\"");// end html at next "
+            html = html.Substring(0, index);
+
+           // System.Diagnostics.Debug.Write("asdfasdf" + html);
+            return html;
+        }
+        private void getJPGButton_Click(object sender, EventArgs e)
+        {
+            string url = urlBox.Text; // the fucking initial url
+           // MessageBox.Show(url);
+            while (true) // loop until can't find anymore urls
+            {
+                //System.Diagnostics.Debug.Write(url + "\n");
+                /*get HTML*/
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // checks if shit worked or not
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                   /* if (response.CharacterSet == null)
+                    {
+                        
+                    }
+                    else
+                    {
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    }*/
+                    readStream = new StreamReader(receiveStream);
+                    string html = readStream.ReadToEnd();
+                    
+                    response.Close();
+                    readStream.Close();
+                    string series = getSeriesName(html);
+                    string chapter = getChapter(html);
+                    string page = getPage(html);
+                    
+                    string extracted = extractJPGFromHTML(html);
+                    //MessageBox.Show(extracted);
+                    downloadJPG(extracted, series, chapter, page);
+                    //MessageBox.Show("getting chapter");
+
+                    url = getNextURL(html);
+                    if (url == "null")
+                    {
+                        MessageBox.Show("Finished");
+                        return;
+                    }
+                       
+
+                }
+
+                else // gotta try different url
+                {
+                    
+                }
+            }
+
+
+           
+        }
+
+        private void urlBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
